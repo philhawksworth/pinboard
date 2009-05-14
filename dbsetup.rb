79acf2b6033f519772a072d22b78edbed7776a0f
philhawksworth <phawksworth@gmail.com>
2009-05-14 11:31:54 +0100

    Added an info footer and made the dbsetup script produce an example board. Mutliple boards support still required.

--- dbsetup.rb
+++ dbsetup.rb
@@ -3,8 +3,9 @@ require 'sqlite3'
 
 db = SQLite3::Database.new("theBoards.db")
 
-# creat the db table.
+# create the db table.
 db.execute("create table boards (name TEXT, data BLOB)")
 
 # create a board. This needs to be done via the UI soon.
-db.execute( "insert into boards (name, data) values ('prospects', 'foo, bar, baz')")
+jsonBlob = '{"title": "Example", "modifier": "Phil Hawksworth", "modified": "14/4/2009 at 11:25", "categories": ["Ideas", "Tasks", "Done", "Shopping", "Notes"], "cards": [{"name": "A smaller idea", "categories": "Ideas"}, {"name": "A big idea", "categories": "Ideas"}, {"name": "That thing I should do", "categories": "Tasks"}, {"name": "That thing I did", "categories": "Done"}, {"name": "Buy milk", "categories": "Shopping"}]}'
+db.execute( "insert into boards (name, data) values ('prospects', jsonBlob)")
