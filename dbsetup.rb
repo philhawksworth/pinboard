7105fbf83bf1f3b96ba4953509b41b958e7b6844
philhawksworth <phawksworth@gmail.com>
2009-05-21 14:46:52 +0100

    Bring all initialisation tasks into the main sinatra file. One stop shop.

--- dbsetup.rb
+++ dbsetup.rb
@@ -3,9 +3,12 @@ require 'sqlite3'
 
 db = SQLite3::Database.new("theBoards.db")
 
-# create the db table.
-db.execute("create table boards (name TEXT, data BLOB)")
+# create the db table if it doesn;t already exist.
+db.execute("create table if not exists boards (name TEXT, data BLOB) ")
 
-# create a board. This needs to be done via the UI soon.
-jsonBlob = '{"title": "example", "modifier": "Phil Hawksworth", "modified": "14/4/2009 at 11:25", "categories": ["Ideas", "Tasks", "Done", "Shopping", "Notes"], "cards": [{"name": "A smaller idea", "categories": "Ideas"}, {"name": "A big idea", "categories": "Ideas"}, {"name": "That thing I should do", "categories": "Tasks"}, {"name": "That thing I did", "categories": "Done"}, {"name": "Buy milk", "categories": "Shopping"}]}'
-db.execute( "insert into boards (name, data) values ('example', '" + jsonBlob + "')")
+# create an example board. if there isn't one already.
+example = db.execute("select * from boards where name='example'")
+if example.size == 0
+  jsonBlob = '{"title": "example", "modifier": "Phil Hawksworth", "modified": "14/4/2009 at 11:25", "categories": ["Ideas", "Tasks", "Done", "Shopping", "Notes"], "cards": [{"name": "A smaller idea", "categories": "Ideas"}, {"name": "A big idea", "categories": "Ideas"}, {"name": "That thing I should do", "categories": "Tasks"}, {"name": "That thing I did", "categories": "Done"}, {"name": "Buy milk", "categories": "Shopping"}]}'
+  db.execute( "insert into boards (name, data) values ('example', '" + jsonBlob + "')")
+end
